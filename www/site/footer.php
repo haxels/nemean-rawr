156b9b4829e437a39de62c8113c18fb4ee54d60a
Havard <havardax@gmail.com>
2014-01-14 22:19:49 +0100

    Add social tiles

--- www/site/footer.php
+++ www/site/footer.php
@@ -9,13 +9,12 @@
 ?>
 
         <div class="push"></div>
-    </div>
-    <footer><br>
+   </div>
+    <footer>
 
-       <div id="navfooter"> <a href="?m=articles&act=view&artID=37"> Kontakt</a> - <a href="?m=articles&act=view&artID=36">Om oss</a> - <a href="https://www.facebook.com/nemeanlan"> Facebook </a> - <a href="https://twitter.com/NemeanLAN"> Twitter </a> - <a href=""> Sponsorer </a> </div>
-      <br> <div id="copyright"> &copy; 2012 Nemean </div>
+       <div id="copyright"> &copy; 2012 Nemean </div>
     </footer>
-
+ 
 <?php if ($session->isAuthorized(array('Developer', 'Crew'))) : ?>
         <a id="toAdminBtn" href="admin.php">Adminpanel</a>
     <?php endif; ?>
