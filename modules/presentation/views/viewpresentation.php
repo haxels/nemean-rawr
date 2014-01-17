2d683af97714faed6b70ee3aac93d6e96444e00e
Havard <havardax@gmail.com>
2014-01-17 18:57:04 +0100

    Update presenter style

--- modules/presentation/views/viewpresentation.php
+++ modules/presentation/views/viewpresentation.php
@@ -3,16 +3,16 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Nemean - Presentasjon</title>
-    <link href="resources/site/html/new/css/reveal.css" rel="stylesheet" type="text/css" />
-    <link href="resources/site/html/new/css/presentation.css" rel="stylesheet" type="text/css" />
+    <link href="site/css/reveal.css" rel="stylesheet" type="text/css" />
+    <link href="site/css/presentation.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script type="text/javascript" src="resources/site/html/new/js/reveal.js"></script>
+    <script type="text/javascript" src="site/js/reveal.js"></script>
 
 </head>
 <body>
 
     <div class="reveal centered">
-        <div class="header"><img src="resources/site/html/admin/img/nemean2013.png" width="15%"/></div>
+        <div class="header"><img src="site/img/logo.jpg" width="12%"/></div>
 
 <hr>
         <div class="slides" id="presentationHolder">
@@ -50,11 +50,7 @@
     </div>
     <div id="sponsorer">
         <hr>
-        <a href="http://www.sparebankenhemne.no"  target="_blank"><img border="none"  src="resources/site/img/sponsorer/sbh.gif" width="200"/></a>
-        <a href="http://www.hemnenett.no"         target="_blank"><img border="none"src="resources/site/img/sponsorer/hemnenett.gif" width="200" /></a>
         
-        <!-- <div id="sponsor"><a href="http://www.lundbakeri.no/"       target="_blank"><img border="none" style="padding-left:7px;" src="resources/site/img/sponsorer/lund.png" width="160" /></a></div>
-        -->
     </div>
 </body>
 </html>
