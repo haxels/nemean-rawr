b3e87fe138a2bea7902d9b3ca3a5133521bff99d
Havard <havardax@gmail.com>
2014-01-17 20:28:58 +0100

    Rename info file

---
+++ www/site/info/index.php
@@ -0,0 +1,4 @@
+<?php
+    //header("Location: ../../?m=presentation&aAct=viewpresentation");
+    header("Location: http://devmean.nemean.no/nemean-rawr/www/?m=presentation&aAct=viewPresentation");
+?>
