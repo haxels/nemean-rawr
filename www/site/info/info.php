2f263cb4dbddfd427e050d191db7e4705fb0e5e5
Havard <havardax@gmail.com>
2014-01-17 20:30:38 +0100

    Remove info.php

--- www/site/info/info.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //header("Location: ../../?m=presentation&aAct=viewpresentation");
-    header("Location: http://devmean.nemean.no/nemean-rawr/www/?m=presentation&aAct=viewPresentation");
-?>
