edeb0cf1240a548af55d1c86213cce78aee39229
Havard <havardax@gmail.com>
2014-01-19 18:14:55 +0100

    Fix 2.

--- system/config.php
+++ system/config.php
@@ -13,20 +13,19 @@
     /**
      * Database parameters
      */
-
-
-    define('DBHOST', 'server.nemean.no');
-    define('DBUSER', 'zebra');
-    define('DBPASS', 'nemean12toft');
-    define('DBNAME', 'nemean');
-	
-    /*
-
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBNAME', 'nemean');
-
+    if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1')
+    {
+        define('DBHOST', 'localhost');
+        define('DBUSER', 'root');
+        define('DBPASS', '');
+        define('DBNAME', 'nemean');
+    }else
+     {
+        define('DBHOST', 'server.nemean.no');
+        define('DBUSER', 'zebra');
+        define('DBPASS', 'nemean12toft');
+        define('DBNAME', 'nemean');
+     }
 /**
      * !!!!!!!!!!!!!!!!!!!
      * DO NOT COMMIT!!!!!!
