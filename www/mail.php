f90bcb0a905b2f42a047ce4eb7017b33ac715928
Havard <havardax@gmail.com>
2014-01-19 19:24:25 +0100

    Mail.php

--- www/mail.php
+++ www/mail.php
@@ -1,7 +1,12 @@
-<?php 
-
+<?php
+ini_set('error_reporting', E_ALL);
 error_reporting(E_ALL);
- mail("havardax@hotmail.com", "The Gathering", "Ska vi farra på TG?", "from: Peder Vollan",);
+ if (mail("havardax@hotmail.com", "The Gathering", "Ska vi farra på TG?", "from: Peder Vollan")) {
+     echo "Jaaaaa";
+ }
+ else {
+     echo "neeeeei";
+ }
 
-
+echo "Hei på deg!";
 ?>
