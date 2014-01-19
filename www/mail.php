aa3390155274564b9135a7a4c07caf00a9f21d13
Havard <havardax@gmail.com>
2014-01-19 18:09:42 +0100

    Add error reporting in mail.php

--- www/mail.php
+++ www/mail.php
@@ -1,7 +1,7 @@
 <?php 
 
-
- mail("havardax@hotmail.com", "The Gathering", "Ska vi farra på TG?", "from: Peder Vollan");
+error_reporting(E_ALL);
+ mail("havardax@hotmail.com", "The Gathering", "Ska vi farra på TG?", "from: Peder Vollan",);
 
 
 ?>
