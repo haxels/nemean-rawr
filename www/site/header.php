9b190c9d20b7bb4caa927c53984c40f8f7a56a3c
Ragnar Kalland <ragnar.kalland@gmail.com>
2014-01-20 23:54:29 +0100

    Updated jquery

--- www/site/header.php
+++ www/site/header.php
@@ -17,9 +17,8 @@ $submenu = $data['menu'] -> getSubMenu();
     <title>Nemean</title>
     
     <!-- jQuery Javascript -->
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="site/js/jquery.min.js"></script>
+    <script type="text/javascript" src="site/js/jquery-ui.min.js"></script>
 
     <script type="text/javascript" src="site/js/visual.js"></script>
     <script type="text/javascript" src="site/js/slides.js"></script>
