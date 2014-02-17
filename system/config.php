13b1d3dfe06b7e1d115edd8e086dea58c714e412
Ragnar Kalland <ragnar.kalland@gmail.com>
2014-02-17 18:36:30 +0100

    Jau.

---
+++ system/config.php
@@ -0,0 +1,27 @@
+<?php
+
+	/**
+	 * !!!!!!!!!!!!!!!!!!!
+	 * DO NOT COMMIT!!!!!!
+	 * !!!!!!!!!!!!!!!!!!!
+	 */
+
+	/**
+	 * Config file
+	 */
+
+	/**
+	 * Database parameters
+	 */
+
+	define('DBHOST', 'server.nemean.no');
+	define('DBUSER', 'zebra');
+	define('DBPASS', 'nemean12toft');
+	define('DBNAME', 'nemean');
+
+	/**
+	 * !!!!!!!!!!!!!!!!!!!
+	 * DO NOT COMMIT!!!!!!
+	 * !!!!!!!!!!!!!!!!!!!
+	 */
+?>
